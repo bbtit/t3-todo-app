6b99dfd7563cd94aa640fd0a3fbfdb1b7fec8709
bbtit <the8butterfly8dream@gmail.com>
2023-04-05 19:04:58 +0900

    create todoRouter

---
+++ src/server/types.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+//  スキーマの定義とバリデーション
+export const createInput = z
+  .string()
+  .min(1, "todo must be at least 1 letter")
+  .max(50, "todo must be 50 letters or less");
+
+export const updateInput = z.object({
+  id: z.string(),
+  text: z
+    .string()
+    .min(1, "todo must be at least 1 letter")
+    .max(50, "todo must be 50 letters or less"),
+});
+
+export const toggleInput = z.object({
+  id: z.string(),
+  is_completed: z.boolean(),
+});
